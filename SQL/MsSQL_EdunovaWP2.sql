d32534723ee5142d4d1fda335d6d22809743ec48
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-16 21:56:42 +0100

    Update MsSQL_EdunovaWP2.sql

--- SQL/MsSQL_EdunovaWP2.sql
+++ SQL/MsSQL_EdunovaWP2.sql
@@ -4,6 +4,8 @@ drop database if exists edunovawp2;
 go
 create database edunovawp2; 
 go
+alter database edunovawp2 collate Croatian_CI_AS;
+go
 use edunovawp2;
 
 create table smjerovi(
@@ -52,8 +54,33 @@ alter table grupe add foreign key (predavacSifra) references predavaci(sifra);
 alter table clanovi add foreign key (grupa) references grupe(sifra);
 alter table clanovi add foreign key (polaznik) references polaznici(sifra);
 
-insert into smjerovi (naziv, brojsati, cijena, upisnina, verificiran) values
-(
+-- OVA NAREDBA UNOSI JEDAN RED U TABLICU
+insert into smjerovi (naziv, brojSati, cijena, upisnina, verificiran)
+values ('Web programiranje', 225, 1325.85, null, 1);
 
-);
+
+-- minimalni unos
+-- primjer unosa dva reda
+insert into smjerovi (naziv) values 
+('Java programiranje'),
+('Seviser');
+
+insert into predavaci (ime, prezime, email) values
+-- 1
+('Tomislav', 'Jakopec', 'tjakopec@gmail.com'),
+-- 2
+('Shaquille','O''Neal','shaki@gmail.com');
+
+insert into polaznici (ime, prezime, email) values
+('Matija','Pavković','matijapavkovic74@gmail,com'),
+('Natalija','Glavaš','natalija-glavas@hotmail.com');
+
+insert into grupe (naziv, smjerSifra, datumPocetka) values
+('WP1', 1, '2023-04-24 17:00:00'),
+('WP2', 1, '2023-10-30 17:00:00'),
+('JP28', 2, '2023-04-24 19:00:00');
+
+insert into clanovi (grupa, polaznik) values
+(2,1),
+(2,2);
 
