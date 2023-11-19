cb1a665ef308ce4357163f07eb90ee1bfb5e2bb8
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-19 20:26:44 +0100

    Update MsSQL_EdunovaWP2.sql

--- SQL/MsSQL_EdunovaWP2.sql
+++ SQL/MsSQL_EdunovaWP2.sql
@@ -48,39 +48,76 @@ create table clanovi(
 grupa int not null,
 polaznik int not null
 );
---komentirao -- dodavanje vanjskih ključeva i povezivanje tablica 
+-- dodavanje vanjskih ključeva i povezivanje tablica 
 alter table grupe add foreign key (smjerSifra) references smjerovi(sifra); 
 alter table grupe add foreign key (predavacSifra) references predavaci(sifra); 
 alter table clanovi add foreign key (grupa) references grupe(sifra);
 alter table clanovi add foreign key (polaznik) references polaznici(sifra);
 
--- OVA NAREDBA UNOSI JEDAN RED U TABLICU
-insert into smjerovi (naziv, brojSati, cijena, upisnina, verificiran)
-values ('Web programiranje', 225, 1325.85, null, 1);
 
+insert into smjerovi (naziv,brojsati,cijena,upisnina,verificiran)
+values ('Web programiranje',225,1325.85,null,1);
 
--- minimalni unos
--- primjer unosa dva reda
-insert into smjerovi (naziv) values 
+insert into smjerovi (naziv) values
 ('Java programiranje'),
-('Seviser');
+('Serviser');
 
-insert into predavaci (ime, prezime, email) values
+insert into predavaci (ime,prezime,email) values
 -- 1
-('Tomislav', 'Jakopec', 'tjakopec@gmail.com'),
+('Tomislav','Jakopec','tjakopec@gmail.com'),
 -- 2
-('Shaquille','O''Neal','shaki@gmail.com');
+('Shaquille', 'O''Neal','shaki@gmail.com');
 
-insert into polaznici (ime, prezime, email) values
-('Matija','Pavković','matijapavkovic74@gmail,com'),
-('Natalija','Glavaš','natalija-glavas@hotmail.com');
 
-insert into grupe (naziv, smjerSifra, datumPocetka) values
-('WP1', 1, '2023-04-24 17:00:00'),
-('WP2', 1, '2023-10-30 17:00:00'),
-('JP28', 2, '2023-04-24 19:00:00');
 
-insert into clanovi (grupa, polaznik) values
-(2,1),
-(2,2);
+insert into polaznici (prezime,ime,email) values
+('Božić','Petra','bozic.petra35@gmail.com'),
+('Farkaš','Dominik','sinisartf13@gmail.com'),
+('Glavaš','Natalija','natalija-glavas@hotmail.com'),
+('Janić','Miroslav','miroslav.janic@gmail.com'),
+('Janješić','Filip','filip.janjesic@gmail.com'),
+('Jović','Nataša','natasajovic238@gmail.com'),
+('Barić','Luka','lukabaric15@gmail.com'),
+('Kelava','Antonio','kelava.antonio392@gmail.com'),
+('Kešinović','Marijan','kesinovic957@gmail.com'),
+('Leninger','Ivan','ileninger@live.com'),
+('Macanga','Antonio','macanga.antonio@gmail.com'),
+('Miloloža','Antonio','milolozaantonio@yahoo.com'),
+('Pavković','Matija','matijapavkovic74@gmail.com'),
+('Peterfaj','Karlo','karlo.peterfaj@gmail.com'),
+('Plečaš','Adriana','adriana.plecas@gmail.com'),
+('Senčić','Ivan','ivan.sencic2000@gmail.com'),
+('Šuler','Zvonimir','zvonimir.suler@gmail.com'),
+('Turček','Mario','mario.turcek@gmail.com'),
+('Veseli','Domagoj','dveseli555@gmail.com'),
+('Vuković','Kristijan','alanford700@gmail.com'),
+('Vukušić','Ivan','ivukusic27@gmail.com'),
+('Žarić','Roman','roman.zaric@gmail.com'),
+('Pavlović','Ivan','ipavlovic6437@gmail.com'),
+('Županić','Andrea','andrea.zupanic10@gmail.com'),
+('Županić','Tomislav','tomislav.zupanic333@gmail.com'),
+('Petak','Martina','petak.martina@gmail.com'),
+('Perak','Marko','markoperak469@gmail.com'),
+('Mokriš','Bartol','bartol567@gmail.com'),
+('Jularić','Ljubomir','ljubojularic93@gmail.com'),
+('Ćelić','Ivor','ivorcelic@gmail.com');
+
+
+
+insert into grupe (naziv,smjerSifra,datumpocetka) values
+-- 1
+('WP1',1,'2023-04-24 17:00:00'),
+-- 2
+('WP2',1,'2023-10-30 17:00:00'),
+-- 3
+('JP28',2,'2023-04-24 19:00:00');
+
+
+insert into clanovi (grupa,polaznik) values
+(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),
+(2,8),(2,9),(2,10),(2,11),(2,12),(2,13),(2,14),
+(2,15),(2,16),(2,17),(2,18),(2,19),(2,20),(2,21),
+(2,22),(2,23),(2,24),(2,25),(2,26),(2,27),(2,28),
+(2,29),(2,30);
+
 
