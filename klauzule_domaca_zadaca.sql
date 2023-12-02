eb15fee77823b6fa19b898c9886d588976dd02e7
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-12-02 18:09:24 +0100

    Update klauzule_domaca_zadaca.sql

--- klauzule_domaca_zadaca.sql
+++ klauzule_domaca_zadaca.sql
@@ -25,7 +25,7 @@ where a.redniBroj = '14778/2017'
 order by 4 desc;
 
 
-select sum (b.kolicina * b.cijena) as ukupniIznoz
+select sum (b.kolicina * b.cijena) as ukupniIznos
 from primke a inner join ArtikliNaPrimci b
 on a.sifra = b.primka
 inner join Artikli c on b.artikl = c.sifra
@@ -46,6 +46,24 @@ order by 3 desc;
 
 -- domaća zadaća pronaći najproduktivnijeg klijenta iz županije
 
+select a.ime, a.prezime, a.jmbg
+from kupci a inner join racuni b on a.sifra = b.kupac;
+-- kupce je nemoguće izdvojiti -- no info available
+select * from racuni;
+select * from kupci; 
+
+select d.naziv, a.naziv, sum(f.kolicina * f.cijena) as ukupniIznos
+from dobavljaci a inner join mjesta b on a.mjesto = b.sifra
+inner join opcine c on b.opcina = c.sifra
+inner join zupanije d on c.zupanija = d.sifra
+inner join primke e on a.sifra = e.dobavljac
+inner join ArtikliNaPrimci f on f.primka = e.sifra
+inner join Artikli g on f.artikl = g.sifra
+where d.naziv = 'Osječko-baranjska županija'
+group by a.naziv, d.naziv
+order by 3 desc;
+
+select * from zupanije;
 -- traženje igle u plastu sijena
 -- obrisati sve artikle koji nisu nikada nabavljeni
 select count(*) from artikli; 
@@ -61,4 +79,5 @@ not in (select distinct artikl from ArtikliNaPrimci);
 
 -- domaća zadaća update i delete pomoću spajanja tablica 
 
-
+merge dobavljaci as target
+using primke as source
