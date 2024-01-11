256c48c975fcbb8a5fb6f7d8aefd8ebf493d41e3
Matija1988 <46812296+Matija1988@users.noreply.github.com>
2024-01-11 18:59:10 +0100

    API metode

--- csharp/WebApi/Controllers/E07Metode.cs
+++ csharp/WebApi/Controllers/E07Metode.cs
@@ -22,8 +22,56 @@ namespace WebApi.Controllers
 
         }
 
+        // kreirati rutu zad2 koja prima 4 cijela broja i vraca razliku 1+2 i 3+4
+        // koristiti kreiranu metodu za zbroj dvaju brojeva
+        [HttpGet]
+        [Route("zad2")]
 
+        public int Zad2(int a, int b, int c, int d) 
+        {
+
+            return Zbroj(a, b) - Zbroj(c, d); 
+        }
+
+
+        //DZ
+
+        //Kreirati rutu ZAD3 koja prima ime grada i slovo. Ruta vraca broj pojavljivanja slova u primljenom imenu grada
+        //Koristiti metode 
+
+        [HttpGet]
+        [Route("zad3")]
+
+        public int Zad3(string grad, string slovo)
+        {
+
+            return BrojSlova(grad, slovo);
+        }
+
+         int BrojSlova(string grad, string slovo)
+        {
+
+            int brojac = 0;
+
+            foreach (char c in grad) 
+            {
+                
+                foreach (char d in slovo)
+                {
+                    if(c == d)
+                    {
+                        brojac++;
+                    }
+                }
+            
+            }
+
+            return brojac; 
+        }
 
 
     }
 }
+
+
+
